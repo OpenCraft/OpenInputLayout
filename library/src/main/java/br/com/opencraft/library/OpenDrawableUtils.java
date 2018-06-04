671051e5907a1027bff78e84724f4cd6fd747fd2
Joao Rutkoski <joao.rutkoski@agibank.com.br>
2018-06-04 11:40:53 -0300

    First commit. Created customized version of TextInputLayou that allows displaying the label at the bottom

---
+++ library/src/main/java/br/com/opencraft/library/OpenDrawableUtils.java
@@ -0,0 +1,46 @@
+package br.com.opencraft.library;
+
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableContainer;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+public class OpenDrawableUtils {
+
+    private static final String LOG_TAG = "DrawableUtils";
+
+    private static Method sSetConstantStateMethod;
+    private static boolean sSetConstantStateMethodFetched;
+
+    private OpenDrawableUtils() {}
+
+    static boolean setContainerConstantState(DrawableContainer drawable,
+                                             Drawable.ConstantState constantState) {
+        // We can use getDeclaredMethod() on v9+
+        return setContainerConstantStateV9(drawable, constantState);
+    }
+
+    private static boolean setContainerConstantStateV9(DrawableContainer drawable,
+                                                       Drawable.ConstantState constantState) {
+        if (!sSetConstantStateMethodFetched) {
+            try {
+                sSetConstantStateMethod = DrawableContainer.class.getDeclaredMethod(
+                        "setConstantState", DrawableContainer.DrawableContainerState.class);
+                sSetConstantStateMethod.setAccessible(true);
+            } catch (NoSuchMethodException e) {
+                Log.e(LOG_TAG, "Could not fetch setConstantState(). Oh well.");
+            }
+            sSetConstantStateMethodFetched = true;
+        }
+        if (sSetConstantStateMethod != null) {
+            try {
+                sSetConstantStateMethod.invoke(drawable, constantState);
+                return true;
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "Could not invoke setConstantState(). Oh well.");
+            }
+        }
+        return false;
+    }
+}
